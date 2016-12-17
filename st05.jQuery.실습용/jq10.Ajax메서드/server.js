3b451f143b37ffdfd9e3eb613b88b23726187235
d-heb <developer.insung.heb@gmail.com>
2016-12-17 11:11:05 +0900

    파일 새로 변경

--- st05.jQuery.실습용/jq10.Ajax메서드/server.js
+++ st05.jQuery.실습용/jq10.Ajax메서드/server.js
@@ -9,9 +9,9 @@ var $fs   = require('fs' );
 var $qs   = require('querystring');
 
 var dataJson = JSON.stringify({
-    'foo'     : 'bar',
+    'username': randomString(10, 'afxfdfyrsu98ccy65ki'),
     'email'   : 'email@mail.com',
-    'password': 'a075d17f3d453073853f813838c15b8023b8c487038436354fe599c3942e1f95'
+    'password': 'a075d17f3d453073853f813838c15c3942e1f95'
 });
 
 var dataXml = '';
@@ -41,19 +41,51 @@ function getParams(request, response, callback) {
 
         request.on('end', function() {
             params = $qs.parse(queryData);
-            callback(params);
+            callback(params, request, response);
         });
     }
     else if (request.method.toUpperCase() === 'GET') {
         params = $url.parse(request.url, true);
         console.log(params.query);
 
-        callback(params);
+        callback(params, request, response);
     }
     else {
         response.writeHead(405, {'Content-Type': 'text/plain'} );
         response.end();
     }
+}
+
+/*
+    make a random sting in javascript
+    http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+ */
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+/*
+ make a random sting in javascript
+ http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+ 사용법:
+ var randomValue = randomString(5);
+ var randomValue = randomString(5, 'PICKCHARSFROMTHISSET');
+ */
+function randomString(len, charSet) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
 }
 
 http.createServer( function(req, res) {
@@ -64,7 +96,7 @@ http.createServer( function(req, res) {
 
     if( urlpath === '/text' && req.method.toUpperCase()==='GET'  ) {
         // 비동기적 읽기
-        var filename = __dirname + '/item.html';
+        var filename = __dirname + '/item.txt';
         $fs.readFile( filename, 'utf8', function (err, data) {
             if (err) {
                 throw err;
@@ -143,13 +175,74 @@ http.createServer( function(req, res) {
     }
 
     if( urlpath === '/login' && req.method.toUpperCase() === 'POST'  ) {
+
+        console.log( ' login ...');
+
+        // DB에서 로그인 검증 처리
+
+        
         res.writeHead(200, {
             'Access-Control-Allow-Origin': '*', /* 크로스 도메인 지원 설정 */
-            'Content-Type': 'application/xml charset=utf-8',
-            'Content-Length': Buffer.byteLength(dataXml)
+            'Content-Type': 'application/json charset=utf-8',
+            'Content-Length': Buffer.byteLength(dataJson)
         });
-        res.write(dataXml);
+        res.write(dataJson);
         res.end();
+    }
+
+
+    if( urlpath === '/logout' && req.method.toUpperCase() === 'POST'  ) {
+
+        if (err) {
+            throw err;
+        }
+
+        console.log( ' login ...');
+
+        
+        // DB에서 로그아웃 검증 처리
+
+
+        res.writeHead(200, {
+            'Access-Control-Allow-Origin': '*', /* 크로스 도메인 지원 설정 */
+            'Content-Type': 'application/json charset=utf-8',
+            'Content-Length': Buffer.byteLength(dataJson)
+        });
+        res.write(dataJson);
+        res.end();
+    }
+
+
+
+
+    if( urlpath === '/board' && req.method.toUpperCase() === 'GET'  ) {
+        var html = '';
+
+        getParams(req, res, function(data){
+            console.log( data );
+
+            var arr = [];
+            for(var i=Number(data.query.start); i<=Number(data.query.end); i=i+1){
+                var obj = {
+                    no: i
+                    , title : 'title' + ("00000000" + i).slice(-8)
+                    , writer : makeid()
+                    , content : randomString(100, i.toString() )
+                };
+
+                arr.push( obj );
+            }
+            var txt = JSON.stringify(  arr );
+
+            res.writeHead(200, "OK", {
+                'Access-Control-Allow-Origin': '*', /* 크로스 도메인 지원 설정 */
+                'Content-Type': 'application/json, charset=utf-8',
+                'Content-Length': Buffer.byteLength( txt )
+            });
+            res.write( txt );
+            res.end();
+
+        });
     }
 
 
@@ -173,4 +266,3 @@ http.createServer( function(req, res) {
 }).listen(3000, 'localhost');
 
 console.log('start server - anonymous function');
-
